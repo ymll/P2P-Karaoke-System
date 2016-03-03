da078d431a8e584179ce0a79ba3bcf6497ee9db2
Lily Yung <ymll@live.hk>
2016-03-04 01:54:02 +0900

    Basic model for karaoke system

---
+++ Code/P2PKaraokeSystem/Model/KaraokeSystemModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2PKaraokeSystem.Model
+{
+    public class KaraokeSystemModel
+    {
+        public PlaybackModel Playback { get; set; }
+        public PlayerViewModel View { get; set; }
+
+        public KaraokeSystemModel()
+        {
+            Playback = new PlaybackModel();
+            View = new PlayerViewModel();
+        }
+    }
+}
---
+++ Code/P2PKaraokeSystem/Model/PlayerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2PKaraokeSystem.Model
+{
+    public class PlayerViewModel : AbstractNotifyPropertyChanged
+    {
+        private bool _isPlaying = false;
+        public bool IsPlaying
+        {
+            get { return _isPlaying; }
+            set { SetField(ref _isPlaying, value, "IsPlaying"); }
+        }
+    }
+}
