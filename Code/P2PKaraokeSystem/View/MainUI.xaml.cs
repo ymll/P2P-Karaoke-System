6f0a9fe0967c55d398a129200618e7adee795433
sonia <soniacym@gmail.com>
2016-03-08 03:16:17 +0800

    Basic UI: 1. With the volume Btn, backward, play, forward btns 2. Color change or image change when the mouse over, leave or click 3. Lyric box

--- Code/P2PKaraokeSystem/View/MainUI.xaml.cs
+++ Code/P2PKaraokeSystem/View/MainUI.xaml.cs
@@ -14,18 +14,107 @@ using System.Windows.Shapes;
 
 namespace P2PKaraokeSystem.View
 {
-    /// <summary>
-    /// Interaction logic for MainUI.xaml
-    /// </summary>
+
     public partial class MainUI : Window
     {
+
+        bool isStopping = true;
+        bool soundOn = true;
+
         public MainUI()
         {
-            InitialComponent();
+            InitializeComponent();
         }
 
-        public void InitialComponent(){
-           
+        //Set Image Source for an image throught Uri
+        private void SetImage(Image imageName, string path)
+        {
+            imageName.BeginInit();
+            imageName.Source = new BitmapImage(new Uri(path, UriKind.RelativeOrAbsolute));
+            imageName.EndInit();
+
+        }
+
+        //Play and Stop Button Enter
+        private void playBtn_MouseEnter(object sender, EventArgs e)
+        {
+            if (isStopping)
+                SetImage(playImg, "pack://application:,,,/View/UIMaterial/Image/play_blue.png");
+            else
+                SetImage(playImg, "pack://application:,,,/View/UIMaterial/Image/stop_blue.png");
+        }
+
+        //Play and Stop Mouse Leave
+        private void playBtn_MouseLeave(object sender, EventArgs e)
+        {
+            if (isStopping)
+                SetImage(playImg, "pack://application:,,,/View/UIMaterial/Image/play.png");
+            else
+                SetImage(playImg, "pack://application:,,,/View/UIMaterial/Image/stop.png");
+        }
+
+        //Play and Stop Mouse Click
+        private void playBtn_Click(object sender, EventArgs e)
+        {
+            if (isStopping)
+                SetImage(playImg, "pack://application:,,,/View/UIMaterial/Image/stop_blue.png");
+            else
+                SetImage(playImg, "pack://application:,,,/View/UIMaterial/Image/play_blue.png");
+
+            isStopping = !isStopping;
+        }
+
+        //Backward Button Enter
+        private void backwardBtn_MouseEnter(object sender, EventArgs e)
+        {
+            SetImage(backwardImg, "pack://application:,,,/View/UIMaterial/Image/backward_blue.png");
+        }
+
+        //Backward Button Mouse Leave
+        private void backwardBtn_MouseLeave(object sender, EventArgs e)
+        {
+            SetImage(backwardImg, "pack://application:,,,/View/UIMaterial/Image/backward.png");
+        }
+
+        //Forward Button Enter
+        private void fastforwardBtn_MouseEnter(object sender, EventArgs e)
+        {
+            SetImage(fastforwardImg, "pack://application:,,,/View/UIMaterial/Image/fastforward_blue.png");
+        }
+
+        //Forward Button Mouse Leave
+        private void fastforwardBtn_MouseLeave(object sender, EventArgs e)
+        {
+            SetImage(fastforwardImg, "pack://application:,,,/View/UIMaterial/Image/fastforward.png");
+        }
+
+        //Sound Button Enter
+        private void soundBtn_MouseEnter(object sender, EventArgs e)
+        {
+            if (soundOn)
+                SetImage(soundImg, "pack://application:,,,/View/UIMaterial/Image/volumeup_blue.png");
+            else
+                SetImage(soundImg, "pack://application:,,,/View/UIMaterial/Image/volumeoff_blue.png");
+        }
+
+        //Sound Button Mouse Leave
+        private void soundBtn_MouseLeave(object sender, EventArgs e)
+        {
+            if (soundOn)
+                SetImage(soundImg, "pack://application:,,,/View/UIMaterial/Image/volumeup.png");
+            else
+                SetImage(soundImg, "pack://application:,,,/View/UIMaterial/Image/volumeoff.png");
+        }
+
+        //Sound Button Mouse Click
+        private void soundBtn_Click(object sender, EventArgs e)
+        {
+            if (soundOn)
+                SetImage(soundImg, "pack://application:,,,/View/UIMaterial/Image/volumeoff_blue.png");
+            else
+                SetImage(soundImg, "pack://application:,,,/View/UIMaterial/Image/volumeup_blue.png");
+
+            soundOn = !soundOn;
         }
     }
 }
