8d5d7ab18a4548fddcb2b80e66d447882d85cf72
AnnNGSY <AnnNGSY@users.noreply.github.com>
2016-04-01 18:27:04 +0800

    use size given instead of length of buffer to send data

--- Code/P2PKaraokeSystem/Network/ClientSendManager.cs
+++ Code/P2PKaraokeSystem/Network/ClientSendManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -7,16 +8,51 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace P2PKaraokeSystem.Network
 {
     public class ClientSendManager : AbstractSendManager
     {
-	private Int32 serverport = 12345;
+        private Int32 serverport = 12345;
         private String ServeripString = "127.0.0.1";
         public ClientSendManager()
         {
+            Form1 inputForm = new Form1();
+          //  inputForm.Show();
+            //msg box to collect server's ip address and port
         }
+        public partial class Form1 : Form
+        {
+            public Form1()
+            {
+                InitializeComponent(); 
+            }
+            private void InitializeComponent()
+            {
+                this.AutoSize = true;
+                this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+                TextBox ipaddr = new TextBox();
+                ipaddr.Text = "127.0.0.1";
+                ipaddr.Location = new Point(15, 15);
+                this.Controls.Add(ipaddr);
+                TextBox portnumber = new TextBox();
+                portnumber.Text = "12345";
+                portnumber.Location = new Point(15, 40);
+                this.Controls.Add(portnumber);
+                Button btn = new Button();            
+                btn.Text = "OK";
+                btn.Location = new Point(25, 65);
+                this.Controls.Add(btn);
+                btn.Click += new EventHandler(btn_Click);
+            }
+            void btn_Click(object sender, EventArgs e)
+            {
+              
+
+            }
+        }
+     
         public ClientSendManager(String Serverip, Int32 setport)
         {
             this.serverport = setport;
@@ -30,7 +66,7 @@ namespace P2PKaraokeSystem.Network
                 TcpClient client = new TcpClient();
                 client.Connect(ServeripString, serverport);
                 NetworkStream networkStream = client.GetStream();
-                networkStream.Write(sendBuffer, 0, sendBuffer.Length);
+                networkStream.Write(sendBuffer, 0, size);
                 networkStream.Flush();
                 client.Close();
             }
@@ -40,22 +76,19 @@ namespace P2PKaraokeSystem.Network
             }
             return 0;
         }
-       // const string broadcastipString = "255.255.255.255";
+        const string broadcastAddress = "255.255.255.255";
+        UdpClient sendingClient;
+        private void InitializeSender(byte[] data)
+        {
+            sendingClient = new UdpClient(broadcastAddress, 12345);
+            sendingClient.EnableBroadcast = true;
+            sendingClient.Send(data, data.Length);
+        }
         // TODO
         public override int SendUDP(byte[] sendBuffer, int from, int size)
-        {   
-			UdpClient udpclient = new UdpClient(ServeripString, serverport);
-            udpclient.Send(sendBuffer, size);
+        {
+
             return 0;
         }
-		/*
-		public override int SendUDP(byte[] sendBuffer, int from, int size)
-        {   
-			sendingClient = new UdpClient(broadcastipString, serverport);
-            sendingClient.EnableBroadcast = true;
-            sendingClient.Send(sendBuffer, size);
-            return 0;
-        }
-		*/
     }
 }
