7de6b41505956fc3e36a7ff646e22623875d7dd0
AnnNGSY <AnnNGSY@users.noreply.github.com>
2016-04-04 11:48:54 +0800

    init code of dataRecvListeners

---
+++ Code/P2PKaraokeSystem/Network/SubtitleReceiveListener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2PKaraokeSystem.Network
+{
+    public class SubtitleReceiveListener : DataReceiveListener
+    {
+        public void OnDataReceived(PacketType packetType, byte[] data)
+        {
+            Console.WriteLine(packetType);
+            Console.WriteLine("SubtitleReceiveListener OnDataReceived\n");
+        }
+    }
+}
