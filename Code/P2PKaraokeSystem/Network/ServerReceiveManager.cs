1c80c2087d7f8d3bce6938d0d2bc54ae01e0cb85
AnnNGSY <AnnNGSY@users.noreply.github.com>
2016-04-07 13:44:21 +0800

    Fix typos

--- Code/P2PKaraokeSystem/Network/ServerReceiveManager.cs
+++ Code/P2PKaraokeSystem/Network/ServerReceiveManager.cs
@@ -1,15 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace P2PKaraokeSystem.Network
 {
     public class ServerReceiveManager : AbstractReceiveManager
-    {
-        // TODO: Implement TCP receiver for server
-        private TcpListener client = null;
+      {      
+        // TODO: Implement TCP receiver for client
+        private TcpListener server = null;
         int bufferSize = 500;
         Int32 port = 12345;
         String LocalipString = "127.0.0.1";
@@ -22,58 +25,65 @@ namespace P2PKaraokeSystem.Network
         }
         public override void StartReceiveTcpPacket()
         {
-        /*
+            /*
              * //call with:
              * new ClientReceiveManager().StartReceiveTcpPacket(); 
              * //in main
              */
             try
             {
-                client = new TcpListener(IPAddress.Parse(LocalipString), port);
-                client.Start();
-                new Thread(ListenServerConnect).Start();              
+                server = new TcpListener(IPAddress.Parse(LocalipString), port);
+                server.Start();
+                new Thread(ListenClientConnect).Start();              
             }
             catch
             {
-              client.Stop();
+              server.Stop();
             }
         }
-        private void ListenServerConnect()
+        private void ListenClientConnect()
         {
-            TcpServer newServer = null;
+            TcpClient newClient = null;
             while (true)
             {            
                 try
                 {
-                    newServer = server.AcceptTcpServer();  
-                    new Thread(recvThread).Start(newServer);                 
+                    newClient = server.AcceptTcpClient();  
+                    new Thread(recvThread).Start(newClient);                 
                 }
                 catch
                 {
                 }         
            }
         }
-        private void recvThread(object userServer)
+        private void recvThread(object userClient)
         {
             try
             {
-                TcpServer  server = (TcpServer)userServer;
-                NetworkStream stream = server.GetStream();
+                TcpClient client = (TcpClient)userClient;
+                NetworkStream stream = client.GetStream();
                 int i;
-                byte[] destData = new byte[bufferSize];
+                byte[] destData ;//= new byte[bufferSize];
                 byte[] recvBuffer = new byte[bufferSize];
                 PacketType packetType;
                 while ((i = stream.Read(recvBuffer, 0, recvBuffer.Length)) != 0)
                 {
-                    Console.WriteLine("buff recv:\n");
+                 /*   Console.WriteLine("buff recv:\n");
                     for (int k = 0; k < i; k++)
-                        Console.Write(Convert.ToChar(recvBuffer[k]));
-                    if (this.ParsePacket(recvBuffer, destData, out packetType))
-                    {
+                        Console.Write(recvBuffer[k]);*/
+
+                    if (this.ParsePacket(recvBuffer, out destData, out packetType))
+                    {             
+                    /*    Console.WriteLine("\npacketType = ");
+                        Console.WriteLine(packetType);
+                        Console.WriteLine("\n");*/
+                      /*  for (int k = 0; k < i-payloadSize; k++)
+                            Console.Write(Convert.ToChar(destData[k]));*/
                         this.NotifyListeners(packetType, destData);
                     }
+
                 } 
-                server.Close(); //
+                client.Close();
             }            
             catch
             {
@@ -81,26 +91,24 @@ namespace P2PKaraokeSystem.Network
         }
         public void StopReceivePacket()
         {
-            client.Stop();//
+            server.Stop();
         }
         Thread receivingThread;
-        }
-
-        // TODO: Implement UDP receiver for server
+        // TODO: Implement UDP receiver for client
         public override void StartReceiveUdpDatagram()
-        {
-            UdpServer server = new UdpServer(12345);
+        { 
+            UdpClient client = new UdpClient(12345);
             ThreadStart start = new ThreadStart(Receiver);
             receivingThread = new Thread(start);
             receivingThread.IsBackground = true;
             receivingThread.Start();
         }
 
-        UdpServer receivingServer;
+        UdpClient receivingClient;
 
         private void InitializeReceiver()
         {
-            receivingServer = new UdpServer(12345);
+            receivingClient = new UdpClient(12345);
             ThreadStart start = new ThreadStart(Receiver);
             receivingThread = new Thread(start);
             receivingThread.IsBackground = true;
@@ -111,10 +119,9 @@ namespace P2PKaraokeSystem.Network
             IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 12345);
             while (true)
             {
-                byte[] data = receivingServer.Receive(ref endPoint);
+                byte[] data = receivingClient.Receive(ref endPoint);
 
             }
-
         }
     }
 }
