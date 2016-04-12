8284ebb4b34f3bdce7c2aa1ddd91beacf945236c
Frances Li <cyli83@yahoo.com.hk>
2016-04-12 14:13:42 +0800

    modify onDataReceive input vars: add ip & port

--- Code/P2PKaraokeSystem/Network/LyricRequestListener.cs
+++ Code/P2PKaraokeSystem/Network/LyricRequestListener.cs
@@ -14,7 +14,7 @@ namespace P2PKaraokeSystem.Network
 
     public class LyricRequestListener : DataReceiveListener
     {
-        public void OnDataReceived(PacketType packetType, byte[] data)
+        public void OnDataReceived(PacketType packetType, byte[] data, String ipAddress, Int32 portNo)
         {
             Console.WriteLine(packetType);
             Console.WriteLine(data.Length);
