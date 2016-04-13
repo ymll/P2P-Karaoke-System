7b903eb71aaa998f7ff4013bad6ec940050a9ce9
AnnNGSY <AnnNGSY@users.noreply.github.com>
2016-04-13 22:31:48 +0800

    add extra byte for request type of P2P feature

--- Code/P2PKaraokeSystem/Network/PlayRequestListener.cs
+++ Code/P2PKaraokeSystem/Network/PlayRequestListener.cs
@@ -21,60 +21,23 @@ namespace P2PKaraokeSystem.Network
             Console.WriteLine(data.Length);
             Console.WriteLine("PlayRequestListener OnDataReceived\n");
             String FilePath = "";
-            for (int i = 0; i < data.Length; i++)
+            byte[] temtype = new byte[4];
+            System.Buffer.BlockCopy(data, 0, temtype, 0, temtype.Length);
+            int reqType = BitConverter.ToInt32(temtype, 0);
+            Console.WriteLine("request type = {0}", reqType); 
+
+
+            for (int i = 4; i < data.Length; i++)
             {                
                 FilePath += Convert.ToChar(data[i]);
             }
             Console.Write("FilePath = ");
             Console.WriteLine(FilePath);
-            if (File.Exists(FilePath))
+            if (!File.Exists(FilePath))
             {
-                //send response to that person
-            }
-            else {
                 return;
             }
 
-  /*          var aviHeaderParser = new P2PKaraokeSystem.PlaybackLogic.AviHeaderParser();   
-            aviHeaderParser.LoadFile(FilePath);
-            Avi.AVISTREAMINFO avi_info = aviHeaderParser.AudioHeaderReader.StreamInfo;
-            Avi.PCMWAVEFORMAT format_info = aviHeaderParser.AudioHeaderReader.FormatInfo;
-   */
-           // aviHeaderParser.UnLoadFile();
-            
-            /*turn Avi.AVISTREAMINFO into byte array  */
-  /*         byte[] byteInfo = new byte[Marshal.SizeOf(avi_info) + Marshal.SizeOf(format_info)];
-            IntPtr ptr;
-            ptr = Marshal.AllocHGlobal(byteInfo.Length);
-            Marshal.StructureToPtr(avi_info, ptr, false);
-            int address = ptr.ToInt32();
-            Marshal.Copy(new IntPtr(address), byteInfo, 0, Marshal.SizeOf(avi_info));
-            Marshal.StructureToPtr(format_info, ptr, false);
-            address = ptr.ToInt32();
-            Marshal.Copy(new IntPtr(address), byteInfo, Marshal.SizeOf(avi_info), Marshal.SizeOf(format_info));  
-            Marshal.FreeHGlobal(ptr);
-
-            byte[] temsize = BitConverter.GetBytes((Int32)Marshal.SizeOf(avi_info)).Take(4).ToArray();
-            byte[] temret = new byte[temsize.Length + byteInfo.Length];
-            System.Buffer.BlockCopy(temsize, 0, temret, 0, temsize.Length);
-            System.Buffer.BlockCopy(byteInfo, 0, temret, temsize.Length, byteInfo.Length);
-            byteInfo = temret;
-      
-            byte[] sendBuffer = new byte[byteInfo.Length + 2];
-            ClientSendManager sender = new ClientSendManager("127.0.0.1", 12345);
-            sender.AddPayload(out sendBuffer, byteInfo, PacketType.MEDIA_INFO);
-            sender.SendTCP(sendBuffer, 0, sendBuffer.Length);
-           
-   
-            Thread.Sleep(100);
-*/
-            /*  debugging: what is sent*/
-            /* for (int i = 0; i < Marshal.SizeOf(avi_info); i++ )
-            {
-                Console.Write(byteInfo[i]);
-            }*/
-            /*Console.WriteLine("debug");*/
-            
            /*  * send the avi file to whom request*/
             string fileName = FilePath;
 
@@ -91,16 +54,12 @@ namespace P2PKaraokeSystem.Network
             byte[] buffer = new byte[1024*10 - 2];
             while (((read = fileStream.Read(buffer, 8, buffer.Length - 8))) > 0)
             {
+
                 byte[] temsize = BitConverter.GetBytes(totalWritten);
                 System.Buffer.BlockCopy(temsize, 0, buffer, 0, temsize.Length);
-
                 Console.WriteLine(file.Length - totalWritten);
                 c3.AddPayload(out senddata, buffer, PacketType.VIDEO_STREAM);
                 Console.WriteLine("send form {0} to {1}", totalWritten, totalWritten + buffer.Length - 8);
-         /*    for (int i = 2; i <(senddata.Length); i++)
-            {
-                Console.Write(senddata[i]);
-            }*/
                 c3.SendTCP(senddata, 0, read+10);
                 totalWritten += read;
             //    Thread.Sleep(150);
--- Code/P2PKaraokeSystem/Network/VideoStreamReceiveListener.cs
+++ Code/P2PKaraokeSystem/Network/VideoStreamReceiveListener.cs
@@ -10,8 +10,7 @@ namespace P2PKaraokeSystem.Network
     public class VideoStreamReceiveListener : DataReceiveListener
     {
         private long upto = 0;
-        private string filename = "";
-        private List<byte[]> UnUsedData;
+
         public void OnDataReceived(PacketType packetType, byte[] data, String ipAddress, Int32 portNo)
         {
    
@@ -24,19 +23,14 @@ namespace P2PKaraokeSystem.Network
                 System.Buffer.BlockCopy(data, 0, temsize, 0, temsize.Length);
                 long startpt = BitConverter.ToInt64(temsize, 0);
                 Console.WriteLine("form {0} to {1}", startpt, startpt + data.Length - 8); 
-          /*  for (int i = 0; i < (data.Length); i++)
-            {
-                       Console.Write(data[i]);
-                   }*/
+
                 fileStream.Seek(startpt, SeekOrigin.Begin);
                 fileStream.Write(data, 8, data.Length - 8);
                 upto += data.Length;
                 Console.Write(upto);
                 Console.WriteLine("");
 
-                //     fileStream.Flush();
-                //      fileStream.Dispose();
-
+                fileStream.Flush();
                 fileStream.Close();
           
         }
